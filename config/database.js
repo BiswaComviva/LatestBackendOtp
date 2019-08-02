343a681ce305975cf2284ccc20863e4bc3de6a6e
BiswaComviva <biswaranjan.comviva@gmail.com>
2019-08-02 23:33:51 +0530

    Heroku changes

--- config/database.js
+++ config/database.js
@@ -4,10 +4,10 @@ const mysql = require('mysql')
 
 const conn = mysql.createConnection({
   connectionLimit: 10,
-  host: 'localhost',
-  user: 'root',
-  password:'1610' ,
-  database: 'mytestdb',
+  host: 'us-cdbr-iron-east-02.cleardb.net',
+  user: 'b4aa30cfec8d83',
+  password:'a48a193c' ,
+  database: 'heroku_058ddfaa6496caf',
   port: 3307
 })
 
